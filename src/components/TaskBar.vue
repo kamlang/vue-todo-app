4f037755cf663ebfc74e3502a30c3e2efa84cb7c
Albin Chatton <714006+kamlang@users.noreply.github.com>
2022-06-27 17:52:21 +0200

    added some css rules so taskbar with it not large than 960px

--- src/components/TaskBar.vue
+++ src/components/TaskBar.vue
@@ -20,14 +20,13 @@ export default {
       auth0User: this.$auth0.user,
       isLoading: this.$auth0.isLoading,
       isAuthenticated: this.$auth0.isAuthenticated,
+      creatingNewTaskList: false,
       selectedTaskList: "",
       taskListToDelete: "",
-      newTaskListName: "",
-      creatingNewTaskList: false,
-      taskListToRename: "",
       draggedIndex: "",
+      newTaskListName: "",
+      taskListToRename: "",
       touchTimerStart: 0,
-      taskListSize: 0,
       isOverFlown: Boolean,
       scrollInterval: Object
     }
@@ -255,144 +254,147 @@ export default {
 
 <template>
   <div tabindex="-1" class="ui left inverted menu">
-    <div v-if="isAuthenticated" class="left menu">
-      <div class="item collapsable">
-        <i class="user icon"></i>
-        {{ auth0User.nickname }}
+    <div class="flex-wrapper">
+      <div v-if="isAuthenticated" class="left menu">
+        <div class="item collapsable">
+          <i class="user icon"></i>
+          {{ auth0User.nickname }}
+        </div>
+        <div
+          v-if="isOverFlown"
+          @mousedown="handleMouseDown(handleScrollLeft)"
+          @mouseup="handleMouseUp()"
+          @touchstart.prevent.stop="handleMouseDown(handleScrollLeft)"
+          @touchend.prevent="handleMouseUp()"
+          @touchmove.prevent
+          class="link item"
+        >
+          <i class="angle left icon"></i>
+        </div>
       </div>
-      <div
-        v-if="isOverFlown"
-        @mousedown="handleMouseDown(handleScrollLeft)"
-        @mouseup="handleMouseUp()"
-        @touchstart.prevent.stop="handleMouseDown(handleScrollLeft)"
-        @touchend.prevent="handleMouseUp()"
-        @touchmove.prevent
-        class="link item"
-      >
-        <i class="angle left icon"></i>
-      </div>
-    </div>
-    <div ref="taskListMenu" v-if="isAuthenticated" class="ui inverted center menu taskmenu">
-      <a
-        tabindex="0"
-        data-test-id="deleteTaskList"
-        class="item"
-        title="Delete a task list."
-        @click="taskListToDelete = selectedTaskList"
-        @keydown.space.prevent="taskListToDelete = selectedTaskList"
-        v-if="selectedTaskList"
-      >
-        <i class="delete icon"></i>
-      </a>
+      <div ref="taskListMenu" v-if="isAuthenticated" class="ui inverted center menu taskmenu">
+        <a
+          tabindex="0"
+          data-test-id="deleteTaskList"
+          class="item"
+          title="Delete selected task list."
+          @click="taskListToDelete = selectedTaskList"
+          @keydown.space.prevent="taskListToDelete = selectedTaskList"
+          v-if="selectedTaskList"
+        >
+          <i class="delete icon"></i>
+        </a>
 
-      <div
-        class="link item taskList"
-        v-for="(taskList,index) in taskListArray"
-        data-test-id="taskListName"
-        @click="setSelectedTaskList(taskList.name)"
-        @dblclick="renameTaskListHandler(taskList.name)"
-        @keydown.space.prevent="setSelectedTaskList(taskList.name)"
-        @keydown.enter="renameTaskListHandler(taskList.name)"
-        @dragover.prevent
-        @dragstart="handleDragStart(index)"
-        @drop.prevent="handleDropOver(index)"
-        @dragenter="handleDragEnter(index)"
-        @dragend="handleDragEnd(index)"
-        @dragexit="taskList.isDragTarget = false"
-        @touchstart.prevent.stop="handleTouchStart(index, taskList.name)"
-        @touchend="handleTouchEnd(index)"
-        :class="[selectedTaskList == taskList.name && 'active', draggedIndex === index ? 'horizontal-shake' : '']"
-        :style="[taskList.isDragTarget ? 'border-left: 2px solid' : 'border-left: 1px']"
-        tabindex="0"
-        draggable="true"
-      >
-        <FadeTransition>
-          <div v-if="taskListToRename === taskList.name" class="ui action input">
-            <input
-              ref="renameTaskListInput"
-              data-test-id="inputNewTaskName"
-              v-model="newTaskListName"
-              type="text"
-              :placeholder="taskList.name"
-              @keydown.esc="taskListToRename = ''"
-              @keydown.delete.stop
-              @keydown.space.stop
-              @keydown.enter.stop="newTaskListName ? updateTaskList() : taskListToRename = ''"
-            />
-            <div
-              v-if="newTaskListName"
-              data-test-id="confirmNewName"
-              class="ui icon button"
-              @click.stop="updateTaskList"
-            >
-              <i class="check icon"></i>
+        <div
+          class="link item taskList"
+          v-for="(taskList,index) in taskListArray"
+          title="You can use drag and drop to reorder task lists."
+          data-test-id="taskListName"
+          @click="setSelectedTaskList(taskList.name)"
+          @dblclick="renameTaskListHandler(taskList.name)"
+          @keydown.space.prevent="setSelectedTaskList(taskList.name)"
+          @keydown.enter="renameTaskListHandler(taskList.name)"
+          @dragover.prevent
+          @dragstart="handleDragStart(index)"
+          @drop.prevent="handleDropOver(index)"
+          @dragenter="handleDragEnter(index)"
+          @dragend="handleDragEnd(index)"
+          @dragexit="taskList.isDragTarget = false"
+          @touchstart.prevent.stop="handleTouchStart(index, taskList.name)"
+          @touchend="handleTouchEnd(index)"
+          :class="[selectedTaskList == taskList.name && 'active', draggedIndex === index ? 'horizontal-shake' : '']"
+          :style="[taskList.isDragTarget ? 'border-left: 2px solid' : 'border-left: 1px']"
+          tabindex="0"
+          draggable="true"
+        >
+          <FadeTransition>
+            <div v-if="taskListToRename === taskList.name" class="ui action input">
+              <input
+                ref="renameTaskListInput"
+                data-test-id="inputNewTaskName"
+                v-model="newTaskListName"
+                type="text"
+                :placeholder="taskList.name"
+                @keydown.esc="taskListToRename = ''"
+                @keydown.delete.stop
+                @keydown.space.stop
+                @keydown.enter.stop="newTaskListName ? updateTaskList() : taskListToRename = ''"
+              />
+              <div
+                v-if="newTaskListName"
+                data-test-id="confirmNewName"
+                class="ui icon button"
+                @click.stop="updateTaskList"
+              >
+                <i class="check icon"></i>
+              </div>
+              <div
+                v-else
+                class="ui icon button"
+                data-test-id="cancelNewName"
+                @click.stop="taskListToRename = ''"
+              >
+                <i class="delete icon"></i>
+              </div>
             </div>
-            <div
-              v-else
-              class="ui icon button"
-              data-test-id="cancelNewName"
-              @click.stop="taskListToRename = ''"
-            >
-              <i class="delete icon"></i>
+            <span v-else>
+              <i class="tasks icon collapsable"></i>
+              {{ taskList.name }} ({{ taskList.tasks.filter(t => !t.completed).length }})
+            </span>
+          </FadeTransition>
+        </div>
+        <a
+          class="item"
+          data-test-id="createNewTaskListButton"
+          title="Create a new task list."
+          tabindex="0"
+          @click="newTaskListHandler"
+          @keydown.space.prevent="newTaskListHandler"
+        >
+          <FadeTransition>
+            <div v-if="creatingNewTaskList" class="ui action input">
+              <input
+                ref="newTaskNameInput"
+                @click.stop
+                data-test-id="createNewTaskListInput"
+                v-model="newTaskListName"
+                type="text"
+                placeholder="New task list name..."
+                @keydown.delete="!newTaskListName && (creatingNewTaskList = false)"
+                @keydown.esc="creatingNewTaskList = false"
+                @keydown.space.stop
+                @keydown.enter="newTaskListName ? createTaskList() : creatingNewTaskList = false"
+              />
+              <div
+                v-if="newTaskListName"
+                data-test-id="validateTaskListNameButton"
+                class="ui icon button"
+                @click.stop="createTaskList"
+              >
+                <i class="check icon"></i>
+              </div>
+              <div v-else class="ui icon button" @click.stop="creatingNewTaskList = false">
+                <i class="delete icon"></i>
+              </div>
             </div>
-          </div>
-          <span v-else>
-            <i class="tasks icon collapsable"></i>
-            {{ taskList.name }} ({{ taskList.tasks.filter(t => !t.completed).length }})
-          </span>
-        </FadeTransition>
+          </FadeTransition>
+          <i v-if="!creatingNewTaskList" class="plus icon"></i>
+        </a>
       </div>
-      <a
-        class="item"
-        data-test-id="createNewTaskListButton"
-        title="Create a new task list."
-        tabindex="0"
-        @click="newTaskListHandler"
-        @keydown.space.prevent="newTaskListHandler"
-      >
-        <FadeTransition>
-          <div v-if="creatingNewTaskList" class="ui action input">
-            <input
-              ref="newTaskNameInput"
-              @click.stop
-              data-test-id="createNewTaskListInput"
-              v-model="newTaskListName"
-              type="text"
-              placeholder="New task list name..."
-              @keydown.delete="!newTaskListName && (creatingNewTaskList = false)"
-              @keydown.esc="creatingNewTaskList = false"
-              @keydown.space.stop
-              @keydown.enter="newTaskListName ? createTaskList() : creatingNewTaskList = false"
-            />
-            <div
-              v-if="newTaskListName"
-              data-test-id="validateTaskListNameButton"
-              class="ui icon button"
-              @click.stop="createTaskList"
-            >
-              <i class="check icon"></i>
-            </div>
-            <div v-else class="ui icon button" @click.stop="creatingNewTaskList = false">
-              <i class="delete icon"></i>
-            </div>
-          </div>
-        </FadeTransition>
-        <i v-if="!creatingNewTaskList" class="plus icon"></i>
-      </a>
-    </div>
-    <div class="ui inverted right menu">
-      <div
-        v-if="isAuthenticated && isOverFlown"
-        @mousedown="handleMouseDown(handleScrollRight)"
-        @mouseup="handleMouseUp()"
-        @touchstart.prevent.stop="handleMouseDown(handleScrollRight)"
-        @touchend.prevent="handleMouseUp()"
-        @touchmove.prevent
-        class="link item"
-      >
-        <i class="angle right icon"></i>
+      <div class="ui inverted right menu">
+        <div
+          v-if="isAuthenticated && isOverFlown"
+          @mousedown="handleMouseDown(handleScrollRight)"
+          @mouseup="handleMouseUp()"
+          @touchstart.prevent.stop="handleMouseDown(handleScrollRight)"
+          @touchend.prevent="handleMouseUp()"
+          @touchmove.prevent
+          class="link item"
+        >
+          <i class="angle right icon"></i>
+        </div>
+        <Authentication></Authentication>
       </div>
-      <Authentication></Authentication>
     </div>
   </div>
   <FadeTransition>
@@ -421,6 +423,18 @@ export default {
   .collapsable {
     display: none !important;
   }
+}
+.flex-wrapper {
+  display: flex;
+  flex: auto;
+  justify-content: center;
+  max-width: min(100%, 960px);
+}
+.menu {
+  justify-content: center;
+}
+.taskmenu {
+  justify-content: flex-start;
 }
 @keyframes horizontal-shaking {
   0% {
