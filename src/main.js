4f72ee4383bba1e9c1016f21cb96479da6c71d93
Albin Chatton <714006+kamlang@users.noreply.github.com>
2022-07-01 21:12:41 +0200

    fixed problem with dotenv

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createAuth0 } from '@auth0/auth0-vue';
-require('dotenv').config()
 const app = createApp(App);
 
 app.use(
   createAuth0({
-    domain: process.env.AUTH0_DOMAIN,
-    client_id: process.env.AUTH0_CLIENT_ID,
+    domain: "dev-vbe2rs37.us.auth0.com",
+    client_id: "IsQUjlKUUTfOwp6Da1SQg8uUL0cHf0Nw",
     redirect_uri: window.location.origin,
-    audience: process.env.AUTH0_AUDIENCE,
+    audience: "htts://glgmsh.com",
     cacheLocation: 'localstorage'
   })
 );
