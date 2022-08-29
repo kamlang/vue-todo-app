ae69134b48895cfffc4d08bbeb3a065ee784e305
Albin Chatton <714006+kamlang@users.noreply.github.com>
2022-08-29 14:36:39 +0200

    refactored a bit

--- src/lib/httpRequest.js
+++ src/lib/httpRequest.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 //const API_URL = "https://192.168.1.6:8443"
 const API_URL = "https://api-todo.glgmsh.com"
 
-export async function httpRequest(accessToken, httpMethod, endpoint, dataSent) {
+export async function httpRequest(accessToken, httpMethod, endpoint, data) {
 
   const config = {
     url: API_URL + endpoint,
     method: httpMethod,
-    data: dataSent,
+    data: data,
     headers: {
       "Authorization": `Bearer ${accessToken}`,
       "Content-type": "application/json; charset=UTF-8"
@@ -16,8 +16,7 @@ export async function httpRequest(accessToken, httpMethod, endpoint, dataSent) {
   }
   try {
     const response = await axios(config)
-    const responseData = await response.data
-    return responseData
+    return await response.data
   } catch (error) {
     const serverErrorWithCustomMessage = error.response.data?.message
     throw Error(serverErrorWithCustomMessage || error)
